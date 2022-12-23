24ea0d9d5710977c077c93893373811eb19fb57f
Gavin <gavinwjw@163.com>
2022-12-23 11:01:09 +0800

    chore: improve notes

--- packages/uniqBy.js
+++ packages/uniqBy.js
@@ -3,7 +3,7 @@ import { test } from './test'
  * @description 调用数组的每个元素以产生唯一性
  * @param {Array} arr 要去重的数组
  * @param {*} it 迭代函数，调用每个元素
- * @returns {boolean}
+ * @returns {Array} 返回新的去重后的数组
  */
 
 export function uniqBy(arr, it) {
