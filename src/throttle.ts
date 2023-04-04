ddfbb2650932dd7ac5ed935e9a34b2b72facdd4e
Gavin <gavinwjw@163.com>
2023-04-04 18:54:49 +0800

    chore: improve code

--- src/throttle.ts
+++ src/throttle.ts
@@ -1,14 +1,16 @@
 /**
  * 创建一个节流函数，在 wait 秒内最多执行 func 一次的函数。
+ * @param {Function} fn 需要节流的函数
+ * @param {number} time 毫秒数
  */
 
-export const throttle = (func: (...args: any[]) => any, time = 0) => {
+export const throttle = (fn: (...args: any[]) => any, time = 0) => {
   let activeTime = 0
 
   return function (this: unknown, ...args: any[]) {
     const current = Date.now()
     if (current - activeTime > time) {
-      Reflect.apply(func, this, args)
+      Reflect.apply(fn, this, args)
       activeTime = Date.now()
     }
   }
