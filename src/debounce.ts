499839425f0843aa14eca8ce14ba360f2abcf2a9
Gavin <gavinwjw@163.com>
2023-04-04 18:54:36 +0800

    feat: support execute immediately

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,11 +1,28 @@
 /**
  * 创建一个 debounced（防抖动）函数，该函数会从上一次被调用后，延迟 wait 毫秒后调用 func 方法
+ * @param {Function} fn 需要防抖的函数
+ * @param {number} ms 毫秒数
+ * @param {immediate} ms 是否立即执行
  */
 
-export const debounce = (fn: (...args: any[]) => any, ms = 0) => {
+export const debounce = (
+  fn: (...args: any[]) => any,
+  ms = 0,
+  immediate = false
+) => {
+  let invoked = false
   let timer: ReturnType<typeof setTimeout>
+
   return function (this: unknown, ...args: any[]) {
     if (timer) clearTimeout(timer)
-    timer = setTimeout(() => fn.apply(this, args), ms)
+    if (immediate && !invoked) {
+      Reflect.apply(fn, this, args)
+      invoked = true
+    } else {
+      timer = setTimeout(() => {
+        Reflect.apply(fn, this, args)
+        invoked = false
+      }, ms)
+    }
   }
 }
