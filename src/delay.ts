1101cab2ad8511d79fbdcd0a0c9dc3c969d64dea
Gavin <gavinwjw@163.com>
2023-07-07 10:50:00 +0800

    feat: add later func

--- src/delay.ts
+++ src/delay.ts
@@ -2,19 +2,17 @@ import { isFunction } from '.'
 
 /**
  * @description 延迟 wait 毫秒后调用 func。 调用时，任何附加的参数会传给func。
- * @param {Function} func 要延迟的函数
- * @param {number} wait 要延迟的毫秒数
- * @param {...*} [args] 会在调用时传入到 func 的参数。
- * @returns {number} 返回计时器 id
  */
 
-export function delay(
-  func: (...args: any[]) => any,
-  wait = 0,
-  ...args: any[]
-): ReturnType<typeof setTimeout> {
+export function delay(func: (...args: any[]) => any, wait = 0, ...args: any[]) {
   if (!isFunction(func)) {
     throw new TypeError('Expected a function')
   }
   return setTimeout(func, wait, ...args)
+}
+
+export function later(delay = 0): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay)
+  })
 }
