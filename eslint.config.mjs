df41e16cbef0a82246f15a0db9eeb773efa3fc80
Gavin <gavinwjw@163.com>
2024-04-06 14:16:30 +0800

    chore: upgrade the config of eslint v9

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+modules.exports = [
+  '@offbeat-ui/eslint-config',
+  {
+    rules: {
+      'unicorn/prefer-number-properties': 'off'
+    },
+    ignores: ['!dist/', '!node_modules/', '*.d.ts']
+  }
+]
